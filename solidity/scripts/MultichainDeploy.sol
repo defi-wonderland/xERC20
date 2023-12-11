43153906f61f11e9afb1718fdbbe829e0980504a
0xGorilla <0xGorilla@defi.sucks>
2023-12-11 22:31:30 +0100

    feat: deployments

--- solidity/scripts/MultichainDeploy.sol
+++ solidity/scripts/MultichainDeploy.sol
@@ -11,11 +11,22 @@ import {ScriptingLibrary} from './ScriptingLibrary/ScriptingLibrary.sol';
 contract MultichainDeploy is Script, ScriptingLibrary {
   uint256 public deployer = vm.envUint('DEPLOYER_PRIVATE_KEY');
   address constant CREATE2 = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
-  string[] public chains = ['MAINNET_RPC', 'POLYGON_RPC', 'OPTIMISM_RPC', 'ARBITRUM_RPC', 'GNOSIS_RPC', 'BSC_RPC'];
+  string[] public chains = [
+    'ETHEREUM_MAINNET_RPC',
+    'POLYGON_MAINNET_RPC',
+    'OPTIMISM_MAINNET_RPC',
+    'ARBITRUM_MAINNET_RPC',
+    'GNOSIS_RPC',
+    'BSC_RPC',
+    'ETHEREUM_GOERLI_RPC',
+    'POLYGON_MUMBAI_RPC',
+    'ARBITRUM_GOERLI_RPC',
+    'OPTIMISM_GOERLI_RPC'
+  ];
 
   function run() public {
     // When new factories need to be deployed, make sure to update the salt version
-    bytes32 _salt = keccak256(abi.encodePacked('xERC20-v1.1', msg.sender));
+    bytes32 _salt = keccak256(abi.encodePacked('xERC20-v1.2', msg.sender));
     address[] memory _factories = new address[](chains.length);
 
     for (uint256 _i; _i < chains.length; _i++) {
@@ -27,7 +38,7 @@ contract MultichainDeploy is Script, ScriptingLibrary {
       address _deployedFactory = getAddress(_bytecodeFactory, _salt, CREATE2);
       XERC20Factory _fact = new XERC20Factory{salt: _salt}();
       require(address(_fact) == _deployedFactory, 'Factory address does not match');
-      
+
       vm.stopBroadcast();
       // solhint-disable-next-line no-console
       console.log(chains[_i], 'factory deployed to:', address(_deployedFactory));
